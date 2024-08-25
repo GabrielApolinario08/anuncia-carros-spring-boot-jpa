74a04f2b3a1db8cab0b1ca6911b34a31e448f12e
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-25 13:03:56 -0300

    criando projeto e configurando dependências

---
+++ src/main/java/com/cars/car_advertisement/CarAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package com.cars.car_advertisement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAdvertisementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAdvertisementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cars/car_advertisement/CarAdvertisementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars.car_advertisement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAdvertisementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
