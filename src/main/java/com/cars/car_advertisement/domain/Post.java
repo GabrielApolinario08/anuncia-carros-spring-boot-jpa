24e607a4ca2bfc9c09ccc57a6b434f2c81924ad5
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-26 11:42:21 -0300

    criando entidade post

---
+++ src/main/java/com/cars/car_advertisement/domain/Post.java
@@ -0,0 +1,22 @@
+package com.cars.car_advertisement.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDateTime date;
+    private String title;
+}
