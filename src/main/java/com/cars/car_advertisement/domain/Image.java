14b2c3e7a7b35f76d63eb76ae0297daf015bdb05
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-26 11:45:23 -0300

    criando entidade image

---
+++ src/main/java/com/cars/car_advertisement/domain/Image.java
@@ -0,0 +1,19 @@
+package com.cars.car_advertisement.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "image")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String url;
+}
