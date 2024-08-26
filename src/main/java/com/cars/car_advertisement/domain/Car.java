6b336d80f0c445a7ed1535871ba618e9d0430ca0
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-26 11:37:45 -0300

    criando entidade car

---
+++ src/main/java/com/cars/car_advertisement/domain/Car.java
@@ -0,0 +1,26 @@
+package com.cars.car_advertisement.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "car")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String model;
+    @Column(nullable = false)
+    private String mark;
+    private Double price;
+    private String description;
+    private Integer year;
+    private Integer travelledDistance;
+    private String color;
+}
